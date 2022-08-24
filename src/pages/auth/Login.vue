c547e2b35a68c3619436fd2c697b7828adfa1566
NahinAhmed28 <nahin.ahmed28@gmail.com>
2022-08-25 05:00:12 +0600

    custom styles and assets imported in index html

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,41 +1,37 @@
 <template>
-  <div
-      class="
-      d-flex
-      flex-column flex-column-fluid
-      bgi-position-y-bottom
-      position-x-center
-      bgi-no-repeat bgi-size-contain bgi-attachment-fixed
-    "
-      style="background-image: url('public/illustrations/sketchy-1/14.png'); background-color: rgba(220,220,229,0.75)"
-  >
-    <div class="container" style="padding-top: 15%" >
-      <div class="row justify-content-center">
-        <div class="col-md-12 col-lg-10">
-          <div class="wrap d-md-flex">
-            <div class="img"  :style="{ backgroundImage: `url(${publicPath}/custom/BdDigitalImage.JPG)` }"></div>
-            <div class="login-wrap p-4 p-md-5">
+  <div>
 
-              <form style="font-family: Arial, Baskerville, monospace; padding: 20%" action="#">
-                <h2 style="text-align: center; font-size:x-large ; color:mediumpurple">ERU Login</h2>
+    <section>
+      <div class="container" style="padding-top: 12rem">
+        <div class="row justify-content-center">
+          <div class="col-md-12 col-lg-10">
+            <div class="wrap d-md-flex">
+              <div class="img" style="background-image: url('../../../public/custom/design/img/Capture.JPG');"></div>
+              <div class="login-wrap p-4 p-md-5">
+<!--<img src="public/custom/design/img/Capture.JPG">-->
+                <form style="font-family: Arial, Baskerville, monospace; padding: 20%" action="dashboard_agent.html">
+                  <h2 style="text-align: center; font-size:x-large ; color:mediumpurple">ERU Login</h2>
 
-                <div class="form-outline mb-1">
-                  <input type="text" id="form2Example11" class="form-control" placeholder="Username"/>
-                </div>
+                  <div class="form-outline mb-1">
+                    <input type="text" id="form2Example11" class="form-control" placeholder="Username"/>
+                  </div>
 
-                <div class="form-outline mb-1">
-                  <input type="password" id="form2Example22" class="form-control" placeholder="Password"/>
-                </div>
+                  <div class="form-outline mb-1">
+                    <input type="password" id="form2Example22" class="form-control" placeholder="Password"/>
+                  </div>
 
-                <div class="text-center 1">
-                  <button class="btn btn-block" style="background-color: #5f7fee;color: white" type="button">Log in</button>
-                </div>
-              </form>
+                  <div class="text-center 1">
+                    <button class="btn btn-block" style="background-color: #5f7fee;color: white" type="button">Log in</button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
+
   </div>
 </template>
 
