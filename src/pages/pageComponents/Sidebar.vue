08e422b4583f9bce0c0429e5b402657bc757fbff
NahinAhmed28 <nahin.ahmed28@gmail.com>
2022-08-26 04:57:16 +0600

    wip

--- src/pages/pageComponents/Sidebar.vue
+++ src/pages/pageComponents/Sidebar.vue
@@ -19,7 +19,7 @@
         <a href="javascript:;" class="has-arrow">
           <div class="parent-icon"><ion-icon name="home-sharp"></ion-icon>
           </div>
-          <div class="menu-title">Maps</div>
+          <div class="menu-title">ম্যাপস</div>
         </a>
         <ul>
           <li> <a href="#"><ion-icon name="ellipse-outline"></ion-icon>আসন</a>
@@ -27,6 +27,31 @@
           <li> <a href="#"><ion-icon name="ellipse-outline"></ion-icon>জেলা</a>
           </li>
           <li> <a href="#"><ion-icon name="ellipse-outline"></ion-icon>বিভাগ</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="javascript:;" class="has-arrow">
+          <div class="parent-icon"><ion-icon name="bag-handle-sharp"></ion-icon>
+          </div>
+          <div class="menu-title">অন্যান্য তথ্য</div>
+        </a>
+        <ul>
+          <li><a href="#"><ion-icon name="ellipse-outline"></ion-icon>ভোটের ফলাফল</a>
+          </li>
+          <li> <a href="#"><ion-icon name="ellipse-outline"></ion-icon>আসন</a>
+          </li>
+          <li> <a href="#"><ion-icon name="ellipse-outline"></ion-icon>জেলা</a>
+          </li>
+          <li> <a href="#"><ion-icon name="ellipse-outline"></ion-icon>বিভাগ</a>
+          </li>
+          <li><a href="#"><ion-icon name="ellipse-outline"></ion-icon>কেন্দ্র</a>
+          </li>
+          <li><a href="#"><ion-icon name="ellipse-outline"></ion-icon>দলের নাম</a>
+          </li>
+          <li><a href="#"><ion-icon name="ellipse-outline"></ion-icon>রাজনৈতিক দলের তথ্য</a>
+          </li>
+          <li><a href="#"><ion-icon name="ellipse-outline"></ion-icon>প্রার্থীর তথ্য</a>
           </li>
         </ul>
       </li>
